774f7e7aa331bdccc503f15e11a63dee3073cbd6
Darko <shibbytemplar@gmail.com>
2022-10-07 18:21:30 +0200

    Auth routes tests added

---
+++ src/test/java/tests/AuthRoutesTests.java
@@ -0,0 +1,42 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AuthRoutesTests extends BaseTest{
+  @Test
+  public void forbidsVisitsToHomeUrlIfNotAuthenticated() {
+    getDriver().get("https://vue-demo.daniel-avellaneda.com/home");
+
+    String expectedResultURL = "https://vue-demo.daniel-avellaneda.com/login";
+    String actualResultURL = getLoginPage().getDriver().getCurrentUrl();
+    Assert.assertEquals(expectedResultURL, actualResultURL);
+  }
+
+  @Test
+  public void forbidsVisitsToProfileUrlIfNotAuthenticated() {
+    getDriver().get("https://vue-demo.daniel-avellaneda.com/profile");
+
+    String expectedResultURL = "https://vue-demo.daniel-avellaneda.com/login";
+    String actualResultURL = getLoginPage().getDriver().getCurrentUrl();
+    Assert.assertEquals(expectedResultURL, actualResultURL);
+  }
+
+  @Test
+  public void forbidsVisitsToAdminCitiesUrlIfNotAuthenticated() {
+    getDriver().get("https://vue-demo.daniel-avellaneda.com/admin/cities");
+
+    String expectedResultURL = "https://vue-demo.daniel-avellaneda.com/login";
+    String actualResultURL = getLoginPage().getDriver().getCurrentUrl();
+    Assert.assertEquals(expectedResultURL, actualResultURL);
+  }
+
+  @Test
+  public void forbidsVisitsToAdminUsersUrlIfNotAuthenticated() {
+    getDriver().get("https://vue-demo.daniel-avellaneda.com/admin/users");
+
+    String expectedResultURL = "https://vue-demo.daniel-avellaneda.com/login";
+    String actualResultURL = getLoginPage().getDriver().getCurrentUrl();
+    Assert.assertEquals(expectedResultURL, actualResultURL);
+  }
+}
